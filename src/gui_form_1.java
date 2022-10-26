abb869b21dc775a54b0d7c8b69879d97356edad8
tomeczek2886 <tomeczek2886@gmail.com>
2022-10-26 09:58:14 +0200

    Temperatura

--- src/gui_form_1.java
+++ src/gui_form_1.java
@@ -4,20 +4,10 @@ import java.awt.event.ActionListener;
 
 public class gui_form_1 extends JFrame {
     private JPanel Panel1;
-    private JButton button1;
-    private JButton button2;
     private JButton konwersjaButton;
     private JLabel textFieldA;
-    private JButton roznicaButton;
     private JTextField textField1;
     private JTextField textField2;
-    private JTextField textField3;
-    private JCheckBox checkBox1;
-    private JTextField textField4;
-    private JLabel inputA;
-    private JLabel inputB;
-    private JLabel DataLabel;
-    double liczbaA, liczbaB, wynik;
 
     public static void main(String[] args) {
         gui_form_1 gui = new gui_form_1();
